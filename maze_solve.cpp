a55b1bbb1578157d99de8acd7f3e773e05f20856
David DaSilva <daviddasilvajr@gmail.com>
2017-10-25 12:58:40 -0400

    Started building Graph
    Added implementation for putting walls into the graph structure bu pushing (r*c) many nodes into the nodes vector, and initializing each node with row and column values. added print function to view structure.
    
    Need to check bounds next

--- maze_solve.cpp
+++ maze_solve.cpp
@@ -12,17 +12,21 @@
 using namespace std;
 
 class Node{
+public:
   int id;
   bool visited;
   vector <int> adj;
 };
 
 class Graph{
-  vector <Node*> nodes;
-  int DFS(int index, int MazeEnd);
+public:
+    vector <Node*> nodes;
+    int DFS(int index);
+    void Print();
 };
 
 int main(int argc, char **argv){
+
     //read in rows and columns
     fstream input;
     char token[1000];
@@ -48,7 +52,20 @@ int main(int argc, char **argv){
 
     cout << endl;
 
-    //grab walls and coordinates
+    //create new instance of graph
+  Graph* g = new Graph;
+
+  //create nodes and adjacency vector the size of the maze
+    Node* n = new Node;
+    for(int i = 0; i < r*c; i++){
+        n = new Node;
+        n->visited = false;
+        n->id = i;
+        g->nodes.push_back(n);
+    }
+
+    //grab walls and coordinates | and print
+    //place them in g initialize
     while(cin >> token){
         cout << token;
         cin >> token;
@@ -57,29 +74,46 @@ int main(int argc, char **argv){
         cin >> token;
         wCol = atoi(token);
         cout << wCol << endl;
+
+        g->nodes[wRow]->adj.push_back(wCol);
+        g->nodes[wCol]->adj.push_back(wRow);
     }
 
-    //create new instance of graph
-  Graph* g = new Graph;
 
-  //create an adjacency vector the size of the maze
-  g->nodes(r*c);
-  //place them in g initialize
+    //g->Print();
+
 
   //fix each nodes of the list
-  //by checking Out Of Bounts and walls
+  //by checking out of bounds and walls
+
+
   // - call DFS(0)*/
+    g->DFS(0);
     return 0;
 
 }
 
-int Graph::DFS(int index, int MazeEnd) {
-    /*/base case 1
+void Graph::Print(){
+    int i , j;
+    Node *n;
+
+    for (i = 0; i < nodes.size(); i++) {
+        n = nodes[i];
+        cout << "Node " << i << ": " << n->id << ":";
+        for (j = 0; j < n->adj.size(); j++) {
+            cout << " " << n->adj[j];
+        }
+        cout << endl;
+    }
+}
+
+int Graph::DFS(int index) {
+    //base case 1
     if(g->nodes->visited == true){return;}
 
     //base case 2
     if(//hit the end of the maze){
       cout << "PATH 0 0 \n";
-    }*/
+    }
 }
 
